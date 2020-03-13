649216770552c5f02dad275227767cc44f6dea9c
nick migel <nickmigel2@gmail.com>
2020-03-13 12:43:18 -0400

    finished mvp

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import { render } from "@testing-library/react"
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+  const { getByText } = render(<App />);
+  const thing = getByText(/womens world cup/i)
 });
